bd030e845c33e4c8679ecb18cc310cefeb2c4dc8
Manuel Padilla <manuelpad@gmail.com>
2025-05-13 22:57:43 -0300

    chore: support loading both .env and .env.local files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import fs from 'fs';
+
+if (fs.existsSync('.env.local')) {
+    dotenv.config({ path: '.env.local' });
+} else {
+    dotenv.config(); // fallback a .env
+}
 
 import { getUTXOs, getDatum, getAssetsUnderPolicy, getAssetHolders } from './helpers/blockfrost';
 
